4785b81ac28d37cf2b6433e86179c92ddf645ec8
James Carr <jcarr@berkeley.edu>
2015-10-29 15:57:28 -0700

    remove comment

--- proj3/inference.py
+++ proj3/inference.py
@@ -109,8 +109,6 @@ class DiscreteDistribution(dict):
         >>> round(samples.count('d') * 1.0/N, 1)
         0.0
         """
-        
-        #self.normalize()
         result = random.random()*self.total()
         dist2 = list(self.items())
         index = 0
